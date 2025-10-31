f07d3acdf12e5630061d8be40601919aa260156d
Geeneth Punchihewa <geenethpunchihewa@gmail.com>
2025-11-01 00:46:39 +0530

    package version issues fix & service-directory with eureka

--- services/notification-service/src/main/java/com/gearup/notificationservice/entity/Notification.java
+++ services/notification-service/src/main/java/com/gearup/notificationservice/entity/Notification.java
@@ -1,11 +1,24 @@
 package com.gearup.notificationservice.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
+import java.time.LocalDateTime;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "notifications", indexes = {
@@ -41,6 +54,7 @@ public class Notification {
     @Column(nullable = false)
     private NotificationPriority priority;
     
+    @Builder.Default
     @Column(nullable = false)
     private Boolean isRead = false;
     
