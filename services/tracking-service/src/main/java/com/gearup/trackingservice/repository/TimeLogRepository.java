005c026b0db5b1ffb80ccd1937f5333ac754983e
Ihansa Sansiluni Leelarathna <haisleelarathna@gmail.com>
2025-11-08 12:48:47 +0530

    chore: Remove trailing whitespace in multiple files for improved code consistency
    - Cleaned up trailing whitespace in StartTimeLogRequest, StopTimeLogRequest, TimeLog, TimeLogRepository, and the SQL migration script.

--- services/tracking-service/src/main/java/com/gearup/trackingservice/repository/TimeLogRepository.java
+++ services/tracking-service/src/main/java/com/gearup/trackingservice/repository/TimeLogRepository.java
@@ -38,3 +38,4 @@ public interface TimeLogRepository extends JpaRepository<TimeLog, Long> {
 
 
 
+
--- services/tracking-service/src/main/resources/db/migration/V5__create_time_log_table.sql
+++ services/tracking-service/src/main/resources/db/migration/V5__create_time_log_table.sql
@@ -31,3 +31,4 @@ COMMENT ON COLUMN time_log.duration_minutes IS 'Calculated duration in minutes w
 
 
 
+
