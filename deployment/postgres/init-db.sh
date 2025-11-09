259dc4af78a1721b81748c0bdd14dc65625ed58e
AsithaUdara <udarauni001@gmail.com>
2025-11-10 02:46:49 +0530

    fix : fix missing service issues and recreate auth service

--- deployment/postgres/init-db.sh
+++ deployment/postgres/init-db.sh
@@ -9,6 +9,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE DATABASE as_automobile_service;
     CREATE DATABASE as_notification_service;
     CREATE DATABASE as_user_auth_service;
+    CREATE DATABASE as_template_service;
     CREATE DATABASE as_chatbot_service;
     CREATE DATABASE as_customer_service;
     CREATE DATABASE as_vehicle_service;
@@ -23,6 +24,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     CREATE USER svc_automobile_service WITH PASSWORD '$AUTOMOBILE_DB_PASSWORD';
     CREATE USER svc_notification_service WITH PASSWORD '$NOTIFICATION_DB_PASSWORD';
     CREATE USER svc_user_auth_service WITH PASSWORD '$USER_AUTH_DB_PASSWORD';
+    CREATE USER svc_template_service WITH PASSWORD '$TEMPLATE_DB_PASSWORD';
     CREATE USER svc_chatbot_service WITH PASSWORD '$CHATBOT_DB_PASSWORD';
     CREATE USER svc_customer_service WITH PASSWORD '$CUSTOMER_DB_PASSWORD';
     CREATE USER svc_vehicle_service WITH PASSWORD '$VEHICLE_DB_PASSWORD';
@@ -37,6 +39,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE as_automobile_service TO svc_automobile_service;
     GRANT ALL PRIVILEGES ON DATABASE as_notification_service TO svc_notification_service;
     GRANT ALL PRIVILEGES ON DATABASE as_user_auth_service TO svc_user_auth_service;
+    GRANT ALL PRIVILEGES ON DATABASE as_template_service TO svc_template_service;
     GRANT ALL PRIVILEGES ON DATABASE as_chatbot_service TO svc_chatbot_service;
     GRANT ALL PRIVILEGES ON DATABASE as_customer_service TO svc_customer_service;
     GRANT ALL PRIVILEGES ON DATABASE as_vehicle_service TO svc_vehicle_service;
@@ -46,6 +49,18 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
     GRANT ALL PRIVILEGES ON DATABASE as_parts_service TO svc_parts_service;
     GRANT ALL PRIVILEGES ON DATABASE as_appointment_service TO svc_appointment_service;
     GRANT ALL PRIVILEGES ON DATABASE as_modification_service TO svc_modification_service;
+EOSQL
+
+# Setup template service database
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "as_template_service" <<-EOSQL
+    -- Grant schema permissions
+    GRANT ALL ON SCHEMA public TO svc_template_service;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO svc_template_service;
+    GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO svc_template_service;
+
+    -- Grant default privileges
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO svc_template_service;
+    ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO svc_template_service;
 EOSQL
 
 # Setup pgvector extension and permissions for automobile service
